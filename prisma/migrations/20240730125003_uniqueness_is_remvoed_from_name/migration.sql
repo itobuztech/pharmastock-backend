805ee8ea9308bc83c56a15d282d73ec9821b0ac5
Sudeep Chowdhury <sudeep@itobuz.com>
2024-07-30 18:21:11 +0530

    bugfix: IOM-142 warehouse stock migration rectified.

---
+++ prisma/migrations/20240730125003_uniqueness_is_remvoed_from_name/migration.sql
@@ -0,0 +1,14 @@
+-- DropIndex
+DROP INDEX "item_name_key";
+
+-- DropIndex
+DROP INDEX "organization_name_key";
+
+-- DropIndex
+DROP INDEX "pharmacy_name_key";
+
+-- DropIndex
+DROP INDEX "sku_sku_key";
+
+-- DropIndex
+DROP INDEX "warehouse_name_key";
