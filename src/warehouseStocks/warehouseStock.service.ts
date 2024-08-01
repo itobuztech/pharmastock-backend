ff8e29c06e69754f77b8060da6437c35d14a2259
Sudeep Chowdhury <sudeep@itobuz.com>
2024-08-01 11:05:32 +0530

    feature: IOM-153 total price issue resolved with guard removed from the organozations resolver.

--- src/warehouseStocks/warehouseStock.service.ts
+++ src/warehouseStocks/warehouseStock.service.ts
@@ -143,8 +143,6 @@ export class WarehouseStockService {
         };
       }
 
-      console.log('searchObject=', searchObject);
-
       let warehouseStocks: any = await this.prisma.warehouseStock.findMany(
         searchObject,
       );
@@ -165,6 +163,15 @@ export class WarehouseStockService {
         });
 
         warehouseStocks = warehouseStocksFinal;
+      } else {
+        warehouseStocks.forEach((wS: any) => {
+          const finalMrp_base_unit = wS?.item?.mrp_base_unit * wS?.final_qty;
+          const finalWholesale_price =
+            wS?.item?.wholesale_price * wS?.final_qty;
+
+          wS['totalMrpBaseUnit'] = finalMrp_base_unit;
+          wS['totalWholesalePrice'] = finalWholesale_price;
+        });
       }
 
       return { warehouseStocks, total: warehouseStocks.length };
